bd9482b99b577481a327ea4a8f7019c7538f8488
Duong Minh Chien <minhchienwikipedia@gmail.com>
2017-09-13 15:46:54 +0700

    Config accesstoken

---
+++ server/boot/startServer.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function(app) {
+  //   console.log('hello');
+  //   var Member = app.models.Member;
+  //   var Role = app.models.Role;
+  //   var RoleMapping = app.models.RoleMapping;
+  //   var ObjectID = RoleMapping.getDataSource().connector.getDefaultIdType();
+  //   RoleMapping.defineProperty('principalId', {
+  //     type: ObjectID,
+  //   });
+  //   RoleMapping.belongsTo(Member, {foreignKey: 'principalId'});
+  //   Member.hasMany(RoleMapping, {foreignKey: 'principalId'});
+  //   Role.hasMany(Member, {through: RoleMapping, foreignKey: 'roleId'});
+};
